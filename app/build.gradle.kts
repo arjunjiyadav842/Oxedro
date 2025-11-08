597970464e7981a3a20dae9668a788159a53c58a
arjunjiyadav842 <arjunjiyadav842@gmail.com>
2025-11-08 17:44:23 +0530

    Updated build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "tech.oxedro"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "tech.oxedro"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
